b941760498d6b23feb1c81a38b4df90e910c2522
hoppercraft <09official123@gmail.com>
2026-06-15 14:00:10 +0545

    connected postgresql,react and django

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
-# Create your views here.
+@api_view(['GET'])
+def test_connection(request):
+    return Response({
+        "status": "success",
+        "message": "Django and React are officially connected!"
+    })
